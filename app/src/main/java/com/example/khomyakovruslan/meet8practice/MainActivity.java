ddd88a7a6f05ea6e67ae89b8e7ebf04f77aeb6c2
khomyakovruslan <azley@mail.ru>
2018-06-25 21:59:19 +0300

    Added some xml

--- app/src/main/java/com/example/khomyakovruslan/meet8practice/MainActivity.java
+++ app/src/main/java/com/example/khomyakovruslan/meet8practice/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.khomyakovruslan.meet8practice;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment1 fragment1 = new Fragment1();
+        fragmentManager.beginTransaction().add(R.id.frame_container_1,fragment1).commit();
+        Fragment2 fragment2 = new Fragment2();
+        fragmentManager.beginTransaction().add(R.id.frame_container_2,fragment1).commit();
+        Fragment3 fragment3 = new Fragment3();
+        fragmentManager.beginTransaction().add(R.id.frame_container_3,fragment1).commit();
     }
 }
