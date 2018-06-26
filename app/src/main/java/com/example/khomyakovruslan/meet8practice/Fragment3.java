244e434935b811b37e635c9221510404b70027ae
Khomyakov Ruslan <azley@mail.ru>
2018-06-26 23:27:14 +0300

    Third task half made, nee to add handler

--- app/src/main/java/com/example/khomyakovruslan/meet8practice/Fragment3.java
+++ app/src/main/java/com/example/khomyakovruslan/meet8practice/Fragment3.java
@@ -1,19 +1,27 @@
 package com.example.khomyakovruslan.meet8practice;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Fragment3 extends Fragment {
+public class Fragment3 extends Fragment implements LoaderManager.LoaderCallbacks<String>  {
+    private static final int LOADER_ID = 10001;
     private RecyclerView mRecyclerView;
     private MyAdapter mAdapter;
     private LinearLayoutManager mManager;
+
+    private Loader<String> mLoader;
 
     @Nullable
     @Override
@@ -32,9 +40,54 @@ public class Fragment3 extends Fragment {
         // here you can add List you need
 
         mAdapter = new MyAdapter();
-        mAdapter.addData("test");
+        //mAdapter.addData("test");
         mRecyclerView.setAdapter(mAdapter);
+
+        mLoader = getActivity().getSupportLoaderManager().initLoader(LOADER_ID,new Bundle(),this);
+        mLoader.forceLoad();
     }
 
 
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        Loader<String> mLoader = null;
+        if (id==LOADER_ID){
+            mLoader = new MyLoader(getActivity());
+        }
+        return mLoader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        //here you can addData
+        mAdapter.addData(data);
+        mAdapter.notifyItemInserted(MyLoader.count);
+        mLoader.forceLoad();
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
+
+    public static class MyLoader extends AsyncTaskLoader<String>{
+        static int count=0;
+
+        public MyLoader(@NonNull Context context) {
+            super(context);
+        }
+
+        @Nullable
+        @Override
+        public String loadInBackground() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Log.d("TAG", "" + count);
+            return "" + count++;
+        }
+    }
 }
